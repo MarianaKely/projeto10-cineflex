9a0e3638f5da232e4c06ef916031259e88d3ebac
MarianaKely <marianakely.r@gmail.com>
2023-03-08 21:16:31 -0300

    page that presents the film collection implemented - page one

--- src/App.js
+++ src/App.js
@@ -1,38 +1,92 @@
 
+// General configuration page and rendering of cineflex - outset
 
-import styled from "styled-components";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import React from "react";
+import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+
+
+// Import files from js pages - outset
 
 
 import Header from "./components/Header";
+import Pageone from "./pages/Pageone";
 
+
+// Import files from js pages - end
 
 
 export default function App() {
+
+
+
+// Constants that enable functionalities on the App.js - outset
+
+
  
+  const [motionpicture, setMotionpicture] = useState(null);
+  const [movie, setMovie] = useState(null);
   
+
+
+// Constants that enable functionalities on the App.js - end
+
+
+// Configuration that enable functionalities on the App.js - outset
+ 
   useEffect(() => {
-    const promise = axios.get(`${URL}/movies/`);
-    promise.then((res) => setMotionpicture(res.data));
-  }, []);
+
+    const promise = axios.get(`https://mock-api.driven.com.br/api/v8/cineflex/movies`);
+    promise.then((parameter) => setMotionpicture(parameter.data));
+
+  },
+
+  [])
+
+
+// Configuration that enable functionalities on the App.js - end
 
 
   return (
+
+
+// Creation configuration of page layout- outset 
+
+
     <>
      
       <BrowserRouter>
+
           <Header/>
           <Container>
+
             <Routes>
-                
+
+            <Route
+                path="/" element={
+                  <Pageone motionpicture={motionpicture} chosenmovie={setMovie} />
+                }
+            />
+            
             </Routes>
+
           </Container>
+
       </BrowserRouter>
+
     </>
-  );
+
+  )
+
+
+
+  // Creation configuration of page layout- end   
+  
+  
 }
 
 // App.js page styling - outset
@@ -48,3 +102,6 @@ font-family: "Roboto", sans-serif;
 `
 
 // App.js page styling - end
+
+
+// General configuration page and  rendering of cineflex - end
---
+++ src/pages/Pageone.js
@@ -0,0 +1,128 @@
+
+// General configuration and rendering the page that presents the film collection - outset
+
+
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+
+// Import images from assets - outset
+
+
+import Loading from '../assets/loading.jpg'
+
+
+// Import images from assets - end
+
+
+
+export default function Pageone ({ motionpicture, chosenmovie }) {
+
+  return (
+
+// Pageone.js layout - outset
+
+
+  <>
+
+    <Containerpageone>
+
+      <Top>Selecione o filme</Top>
+      <Motionpicturebox>
+
+        {motionpicture === null ? <img src={Loading}></img> : motionpicture.map((parameter) => (
+          
+              <Link to={`/sessoes/${parameter.id}`} key={parameter.id} data-test="movie">
+
+                <Moviebox onClick={() => chosenmovie(parameter)}>
+                  <img src={parameter.posterURL} alt={parameter.title} />
+                </Moviebox>
+
+              </Link>
+
+            ))}
+
+      </Motionpicturebox>
+
+    </Containerpageone>
+
+  </>
+
+  )
+
+
+// Pageone.js layout - end
+
+
+}
+
+
+
+// Pageone.js styling - outset
+
+
+const Containerpageone = styled.div`
+margin-top: 25px;
+margin-left: -8px;
+
+`
+
+// Styling the top title - outset
+
+
+const Top = styled.div`
+width: 100%;
+height: 110px;
+display: flex;
+justify-content: center;
+align-items: center;
+color: #293845;
+font-family: "Roboto", sans-serif;
+font-size: 34px;
+`
+
+// Styling the top title - end
+
+
+// Styling the box that holds all movies images - outset
+
+
+const Motionpicturebox = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+align-items: center;
+`
+
+// Styling the box that holds all movies images - end
+
+
+// Styling the box that holds each movie image - outset
+
+
+const Moviebox = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
+position: relative;
+margin-left: 15px;
+margin-right: 15px;
+margin-bottom: 27px;
+> img {
+width: 129px;
+height: 193px;
+cursor: pointer;
+}
+`
+
+// Styling the box that holds each movie image - end
+
+
+
+// Pageone.js styling - end
+
+
+
+// General configuration and rendering the page that presents the film collection - end
