0029476dcf6ef4b0afbeba5823b3a93fc03b9028
MarianaKely <marianakely.r@gmail.com>
2023-03-09 21:06:07 -0300

    footer code update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,74 @@
+
+// General configuration and rendering the footer - outset
+
+
 import styled from "styled-components";
 import { useLocation } from "react-router-dom";
 import React from "react";
 
+
 export default function Footer({ down, motionpictureroom }) {
+
+
+// Constants that enable functionalities on footer - outset  
+
+
     const { pathname } = useLocation();
+
     if (
-        down &&
-      (pathname.includes("sessoes") || pathname.includes("assentos"))
+        down && (pathname.includes("sessoes") || pathname.includes("assentos"))
+
     ) {
+
+
+// footer only appears on pages two and three        
+
+
+// Constants that enable functionalities on footer - end   
+
+
       return (
+
+
+// Creation configuration of footer layout- outset 
+
+
         <Baseboard data-test="footer">
+
           <BaseboardIMG>
             <img src={down.posterURL} alt={`poster for ${down.title}`} />
           </BaseboardIMG>
+
           <BaseboardText>
             <div>{down.title}</div>
             {motionpictureroom && <div>{`${motionpictureroom.weekday} - ${motionpictureroom.name}`}</div>}
           </BaseboardText>
+
         </Baseboard>
-      );
+
+      )
+
+
+// Creation configuration of header layout- end  
+
+
     }
-    return null;
+
+    return null
+
   }
+
+
+ // Footer.js page styling - outset
+
+
+ // Styling the footer white bar - outset
+
 
 const Baseboard = styled.div`
   height: 117px;
   max-width: 614px;
-    Width:100%;
+  Width:100%;
   position: fixed;
   bottom: 0;
   z-index: 1;
@@ -35,6 +77,12 @@ const Baseboard = styled.div`
   align-items: center;
   
 `
+
+ // Styling the footer white bar - end
+
+
+ // Styling the footer image - outset
+
 
 const BaseboardIMG = styled.div`
   height: 100%;
@@ -49,6 +97,19 @@ const BaseboardIMG = styled.div`
   }
 `
 
-const BaseboardText = styled.text`
+ // Styling the footer image - end
+
+
+// Styling the footer text - outset
+
+const BaseboardText = styled.div`
  font-weight: bold;
 `
+
+// Styling the footer text - end
+
+
+ // Footer.js page styling - end
+
+
+ // General configuration and rendering the footer - end
